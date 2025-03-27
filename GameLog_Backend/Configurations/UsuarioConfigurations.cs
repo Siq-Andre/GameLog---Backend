a563954af39f045884848fb5389fa1e16a0684c3
Siq-Andre <siq.andre2302@gmail.com>
2025-03-27 19:26:32 -0300

    Criacao a configuração de entidades

---
+++ GameLog_Backend/Configurations/UsuarioConfigurations.cs
@@ -0,0 +1,42 @@
+﻿using GameLog_Backend.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace GameLog_Backend.Configurations
+{
+    public class UsuarioConfigurations : IEntityTypeConfiguration<Usuario>
+    {
+        public void Configure(EntityTypeBuilder<Usuario> builder)
+        {
+            builder.HasKey(x => x.Id);
+
+            builder.Property(e => e.Id)
+                .HasColumnName($"{nameof(Usuario)}Id")
+                .ValueGeneratedOnAdd()
+                .IsRequired();
+
+            builder.Property(p => p.NomeUsuario)
+                .HasMaxLength(20)
+                .IsRequired();
+
+            builder.Property(p => p.Email)
+                .HasMaxLength(30)
+                .IsRequired();
+
+            builder.Property(p => p.Senha)
+                .HasMaxLength(128)
+                .IsRequired();
+
+            builder.Property(p => p.FotoDePerfil)
+                .HasColumnType("varchar(MAX)")
+                .IsRequired();
+
+            builder.HasMany(e => e.Avaliacoes)
+                .WithOne()
+                .IsRequired();
+
+            builder.Property(p => p.EstaAtivo)
+                .IsRequired();
+        }
+    }
+}
