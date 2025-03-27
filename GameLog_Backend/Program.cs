d507257c7a5e24cdfc42d5fda5d0f4e7a325870b
Siq-Andre <siq.andre2302@gmail.com>
2025-03-27 19:27:29 -0300

    Configuracoes de conexao com o banco de dados

--- GameLog_Backend/Program.cs
+++ GameLog_Backend/Program.cs
@@ -1,8 +1,19 @@
+using GameLog_Backend.Database;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler = System.Text.Json.Serialization.ReferenceHandler.Preserve;
+    });
+
+builder.Services.AddDbContext<GameLogContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
